7443d457c6952424937f43cb68295787cd1041bd
Kiwikorna <kiwi.jkldf@mail.ru>
2024-09-08 23:37:06 +0300

    1.1

--- Assets/Item/Script/DropItem.cs
+++ Assets/Item/Script/DropItem.cs
@@ -8,36 +8,44 @@ public class DropItem : MonoBehaviour
     [SerializeField] private InventoryInputUI inventoryInputUI;
     [SerializeField] private LayerMask dropItemLayer;
     [SerializeField] private float customDropDistance;
+
     private void Awake()
     {
         Controller.Instance.OnDropItemButtonPressed += InputRemoveItem;
-        
+
     }
+
     private void OnDisable()
     {
         Controller.Instance.OnDropItemButtonPressed -= InputRemoveItem;
     }
+
     private void InputRemoveItem()
     {
         RemoveItem();
     }
+
     private void Update()
     {
         TryRemoveItem();
     }
+
     public bool TryRemoveItem()
     {
         if (inventoryInputUI.GetSelectSlot() < 0 ||
             inventoryInputUI.GetSelectSlot() >= inventoryInputUI.GetSlotUI().Length)
             return false;
+
         InventoryItem slotInItem = GetItemInSlot();
         if (slotInItem == null)
             return false;
         if (!IsHaveEmptySpaceInForwardDirection())
             return false;
+
         return true;
 
     }
+
     public bool RemoveItem()
     {
         InventoryItem slotInItem = GetItemInSlot();
@@ -45,16 +53,16 @@ public class DropItem : MonoBehaviour
         {
             if (IsHaveEmptySpaceInForwardDirection())
             {
-                var item = GetItemInventory();
-                    ShrinkingObjects();
-                    SpawnDropItem(item);
+                var item = slotInItem.GetItemObject();
+                ShrinkingObjects();
+                SpawnDropItem(item);
             }
             else
             {
-                    var item = GetItemInventory();
-                    var dropPosition = GetDropPosition();
-                    ShrinkingObjects();
-                    SpawnDropItem(item, dropPosition);
+                var item = slotInItem.GetItemObject();
+                var dropPosition = GetDropPosition();
+                ShrinkingObjects();
+                SpawnDropItem(item, dropPosition);
             }
 
             return true;
@@ -66,8 +74,9 @@ public class DropItem : MonoBehaviour
     public void SpawnDropItem(ItemObject item)
     {
         Vector3 drop = GetDropPosition();
-        Instantiate(item.prefab,  drop, Quaternion.identity);
+        Instantiate(item.prefab, drop, Quaternion.identity);
     }
+
     public void SpawnDropItem(ItemObject item, Vector3 dropPosition)
     {
         Instantiate(item.prefab, dropPosition, Quaternion.identity);
@@ -76,48 +85,33 @@ public class DropItem : MonoBehaviour
     public bool IsHaveEmptySpaceInForwardDirection()
     {
         float dropDistance = 1f;
-        Ray ray = new Ray(transform.position, transform.forward);
-        bool canDrop = !Physics.Raycast(ray, dropDistance * customDropDistance, dropItemLayer);
+        bool canDrop = !Physics.Raycast(transform.position, transform.forward, dropDistance * customDropDistance,
+            dropItemLayer);
         return canDrop;
     }
 
     private Vector3 GetDropPosition()
     {
         if (IsHaveEmptySpaceInForwardDirection())
-        {
             return player.transform.position + player.transform.forward * distance;
-        }
         else
-        {
             return player.transform.position;
-        }
-    }
-
-    public ItemObject GetItemInventory()
-    {
-        InventoryItem slotInItem = GetItemInSlot();
-        ItemObject item = slotInItem.GetItemObject();
-        return item;
     }
 
     public InventoryItem GetItemInSlot()
     {
         InventorySlot selectedSlot = inventoryInputUI.GetSlotUI()[inventoryInputUI.GetSelectSlot()];
         InventoryItem slotInItem = selectedSlot.GetComponentInChildren<InventoryItem>();
-
         return slotInItem;
     }
+
     private void ShrinkingObjects()
     {
         InventoryItem slotInItem = GetItemInSlot();
         slotInItem.count--;
         if (slotInItem.count <= 0)
-        {
             Destroy(slotInItem.gameObject);
-        }
         else
-        {
             slotInItem.RefreshCount();
-        }
     }
 }
