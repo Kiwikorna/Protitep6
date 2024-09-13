97cbbc813082104e7b7eb055bb214549ea8d3f73
Kiwikorna <kiwi.jkldf@mail.ru>
2024-09-13 22:54:05 +0300

    Delay spell and Cooldown spell

--- Assets/Player/Script/Attack/PlayerSpell.cs
+++ Assets/Player/Script/Attack/PlayerSpell.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.Serialization;
 
 public class SpellCaster : MonoBehaviour
@@ -7,18 +10,42 @@ public class SpellCaster : MonoBehaviour
    [SerializeField] private Transform spellSpawnPoint;
    [SerializeField] private SpellSO playerSpellSo;
    [SerializeField] private CharacterManaSO playerMana;
+   [SerializeField] private float spellDelayPressButton;
+   [SerializeField] private float spellCooldownAfterUsed;
+
+   private bool _isCastingSpell = false;
+   private bool _isCooldownSpell = false;
 
    private void Update()
    {
       if (playerMana.manaCharacter > playerSpellSo.manaCost)
       {
-         if (Input.GetKeyDown(KeyCode.Space))
+         if (Input.GetKeyDown(KeyCode.Space) && !_isCastingSpell && !_isCooldownSpell)
          {
-            var spell = Instantiate(playerSpellSo.prefab, spellSpawnPoint.position, spellSpawnPoint.rotation);
-            spell.GetComponent<Rigidbody>().linearVelocity = spellSpawnPoint.forward * playerSpellSo.speed;
-
-            playerMana.manaCharacter -= playerSpellSo.manaCost;
+            StartCoroutine(DelaySpell());
          }
       }
+   }
+
+   private IEnumerator DelaySpell()
+   {
+      _isCastingSpell = true;
+      yield return new WaitForSeconds(spellDelayPressButton);
+      var spell = Instantiate(playerSpellSo.prefab, spellSpawnPoint.position, spellSpawnPoint.rotation);
+      spell.GetComponent<Rigidbody>().linearVelocity = spellSpawnPoint.forward * playerSpellSo.speed;
+      playerMana.manaCharacter -= playerSpellSo.manaCost;
+      _isCastingSpell = false;
+      StartCoroutine(CooldownSpell());
+      
+      
+      
+   }
+
+   private IEnumerator CooldownSpell()
+   {
+      _isCooldownSpell = true;
+      yield return new WaitForSeconds(spellCooldownAfterUsed);
+
+      _isCooldownSpell = false;
    }
 }
--- Assets/Player/Script/Attack/SpellProjectile.cs
+++ Assets/Player/Script/Attack/SpellProjectile.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class SpellProjectile : MonoBehaviour
 {
-  
+
     [SerializeField] private SpellSO playerDamage;
     [SerializeField] private float spellDistance;
     private Vector3 _startPosition;
@@ -15,11 +15,11 @@ public class SpellProjectile : MonoBehaviour
 
     private void Update()
     {
-       
-       float distance = Vector3.Distance(transform.position, _startPosition);
-       if(distance >= spellDistance)
-           Destroy(gameObject);
-        
+
+        float distance = Vector3.Distance(transform.position, _startPosition);
+        if (distance >= spellDistance)
+            Destroy(gameObject);
+
     }
 
     private void OnTriggerEnter(Collider other)
