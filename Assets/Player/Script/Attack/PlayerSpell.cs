1c7a9f355a0a30f72157f4b6f2fb6e26dc9167be
Kiwikorna <kiwi.jkldf@mail.ru>
2024-09-14 23:23:10 +0300

    Ref code

--- Assets/Player/Script/Attack/PlayerSpell.cs
+++ Assets/Player/Script/Attack/PlayerSpell.cs
@@ -11,16 +11,14 @@ public class SpellCaster : MonoBehaviour
    [SerializeField] private SpellSO playerSpellSo;
    [SerializeField] private CharacterManaSO playerMana;
    [SerializeField] private float spellDelayPressButton;
-   [SerializeField] private float spellCooldownAfterUsed;
 
    private bool _isCastingSpell = false;
-   private bool _isCooldownSpell = false;
 
    private void Update()
    {
       if (playerMana.manaCharacter > playerSpellSo.manaCost)
       {
-         if (Input.GetKeyDown(KeyCode.Space) && !_isCastingSpell && !_isCooldownSpell)
+         if (Input.GetKeyDown(KeyCode.Space) && !_isCastingSpell)
          {
             StartCoroutine(DelaySpell());
          }
@@ -35,17 +33,7 @@ public class SpellCaster : MonoBehaviour
       spell.GetComponent<Rigidbody>().linearVelocity = spellSpawnPoint.forward * playerSpellSo.speed;
       playerMana.manaCharacter -= playerSpellSo.manaCost;
       _isCastingSpell = false;
-      StartCoroutine(CooldownSpell());
-      
-      
-      
    }
 
-   private IEnumerator CooldownSpell()
-   {
-      _isCooldownSpell = true;
-      yield return new WaitForSeconds(spellCooldownAfterUsed);
-
-      _isCooldownSpell = false;
-   }
+   
 }
