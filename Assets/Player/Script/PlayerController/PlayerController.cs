275638883138530080c63a7a2fbc05890e131af7
VladB <vladislav.b@bimiboo.com>
2024-09-16 19:52:47 +0300

    Fixes

--- Assets/Player/Script/PlayerController/PlayerController.cs
+++ Assets/Player/Script/PlayerController/PlayerController.cs
@@ -29,67 +29,28 @@ public class PlayerController : MonoBehaviour
     {
         HandlePlayer();
     }
- 
+
     private void HandlePlayer()
     {
         _move = Controller.Instance.GetDirection();
+        if (_move == Vector2.zero) return;
+
         Vector3 moveDir = new Vector3(_move.x, 0f, _move.y); // Нормализация направления движения
+        _body.AddForce(moveDir * (speed * Time.fixedDeltaTime), ForceMode.Acceleration);
 
-        float playerRadius = 0.5f;
-        float playerHeight = 2f;
-        float distance = speed * Time.deltaTime;
-        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, distance);
+        Quaternion rotate = Quaternion.LookRotation(moveDir, Vector3.up);
+        _body.rotation = Quaternion.Slerp(_body.rotation, rotate, rotationSpeed * Time.deltaTime);
+    }
 
-        if (!canMove)
-        {
-            // Проверка движения только по оси X
-            Vector3 dirX = new Vector3(moveDir.x, 0f, 0f);
-            canMove = (moveDir.x <-.5f || moveDir.x > +.5f) && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, dirX, distance);
-            if (canMove)
-            {
-                moveDir = dirX;
-            }
-            else
-            {
-                // Проверка движения только по оси Z
-                Vector3 dirZ = new Vector3(0f, 0f, moveDir.z);
-                canMove = (dirZ.z <-.5f || dirZ.z > +.5f) && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, dirZ, distance);
-                if (canMove)
-                {
-                    moveDir = dirZ;
-                }
-                else
-                {
-                    
-                }
-            }
-            
-           
-            
-        }
-
-        if (canMove)
-        {
-            _body.linearVelocity = new Vector3(moveDir.x, _body.linearVelocity.y, moveDir.z) * speed;
-            if (moveDir != Vector3.zero)
-            {
-                Quaternion rotate = Quaternion.LookRotation(moveDir, Vector3.up);
-                _body.rotation = Quaternion.Slerp(_body.rotation, rotate, rotationSpeed * Time.deltaTime);
-            }
-        }
-        else
-        {
-            _body.linearVelocity = new Vector3(0, _body.linearVelocity.y, 0);
-            
-
-        }
-
+    private void Update()
+    {
         isRun = _body.linearVelocity != Vector3.zero;
     }
 
     public bool GetIsRun() => isRun;
+
     public void SetSpeed(float value)
     {
-         speed = value;
+        speed = value;
     }
 }
